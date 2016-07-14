67d3b0f18c0b9395a9dc6a7759a32b639d1a4b37
umutuzgur <umut.uzgur@ozu.edu.tr>
2016-07-14 15:21:39 +0200

    more logging

--- src/test/java/ReferenceIosNativeTest.java
+++ src/test/java/ReferenceIosNativeTest.java
@@ -40,9 +40,10 @@ public class ReferenceIosNativeTest {
 		capabilities.setCapability("testobject_api_key", System.getenv("TESTOBJECT_API_KEY")); // API key through env variable
 		capabilities.setCapability("testobject_device", System.getenv("TESTOBJECT_DEVICE_ID")); // device id through env variable
 		capabilities.setCapability("testobject_appium_version", "1.5.2");
-		
-		this.startTime = System.currentTimeMillis();
+
+		long allocationTime = startTime = System.currentTimeMillis();
 		driver = new IOSDriver(new URL("https://app.testobject.com:443/api/appium/wd/hub"), capabilities);
+		System.out.println("Device allocation took: " + (System.currentTimeMillis() - allocationTime));
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
 
@@ -71,10 +72,12 @@ public class ReferenceIosNativeTest {
 
 	@After
 	public void tearDown() {
-		System.out.println("Test took: " + (System.currentTimeMillis() - startTime));
+		long tearDown = System.currentTimeMillis();
 		if (driver != null) {
 			driver.quit();
 		}
+		System.out.println("Driver quit took: " + (System.currentTimeMillis() - tearDown));
+		System.out.println("The whole test took: " + (System.currentTimeMillis() - startTime));
 	}
 
 }
